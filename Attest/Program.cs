0b0dc8e520e1422ca5b27e0f844fd852237355c9
Dinar2023 <dinar-bogdanov@yandex.ru>
2024-01-02 16:53:04 +0500

    1-st version 02.01.2024

---
+++ Attest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrinStrinArray (string[] txt)
+{
+    for (int i=0; i<txt.Length; i++) 
+    {
+        if (txt[i].Length<4) Console.Write($"{txt[i]} ");
+    }
+}
+
+Console.WriteLine("Введите текст");
+string[] someText = Console.ReadLine().Split(" ").ToArray();
+//string[] someText = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+PrinStrinArray(someText);
