d279c5bde23eaaac22d589a96f6d4fb76c48ab13
Dinar2023 <dinar-bogdanov@yandex.ru>
2024-01-03 19:12:10 +0500

    add solve with use Array, remove empty cells

--- Attest/Program.cs
+++ Attest/Program.cs
@@ -1,13 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-void PrinStrinArray (string[] txt)
+﻿// аписать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+string[] NewArray3 (string[] txt)
 {
+    
+    int n = 0;
+    string[] NewStr = new string[txt.Length];
     for (int i=0; i<txt.Length; i++) 
     {
-        if (txt[i].Length<4) Console.Write($"{txt[i]} ");
+        if (txt[i].Length<4) 
+        {
+            NewStr[n] = txt[i];
+            //Console.WriteLine(NewStr[n]);
+            n++;
+        }
     }
+    NewStr = NewStr.Where(x => x != null).ToArray();
+    return (NewStr);
 }
 
 Console.WriteLine("Введите текст");
 string[] someText = Console.ReadLine().Split(" ").ToArray();
-//string[] someText = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-PrinStrinArray(someText);
+string[] NewArray = NewArray3(someText);
+Console.WriteLine(string.Join(", ", NewArray));
